3837b1f5c8ef9b9637d79c86464a7a0c00ae1eb7
unknown <satishnamu2328@gmail.com>
2024-03-26 11:06:07 +0530

    added html pages using jinja

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import  Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 app = Flask(__name__)
+Bootstrap(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 from models.user import User
-from models.course import Course
 
 @app.route('/')
-
-
 def hello_world():
     rec = db.get_or_404(User,1)
-    return f"HELLO WORLD.! from {rec.username} "
+    return render_template("index.html",user=rec)
 
+@app.route('/users')
+def users():
+    user_recs = db.session.query(User).all()
+    users = list(map(lambda rec: rec.__dict__, user_recs))
+    return render_template('users.html', users=users)
